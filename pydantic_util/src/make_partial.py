9b93f33ede81e1f6c5e2846e6d75b683318078cb
mj <kwj4479@gmail.com>
2025-04-02 22:15:01 +0900

    fix: Update pydantic make partial - use default value

--- pydantic_util/src/make_partial.py
+++ pydantic_util/src/make_partial.py
@@ -3,12 +3,17 @@ from typing import Any, Optional, Tuple, Type, TypeVar
 
 from pydantic import BaseModel, create_model
 from pydantic.fields import FieldInfo
+from pydantic_core import PydanticUndefined
 
 
 def _make_field_optional(field: FieldInfo, default: Any = None) -> Tuple[Any, FieldInfo]:
     """Make a field optional"""
     new = deepcopy(field)
-    new.default = default
+    if default == PydanticUndefined:
+        new.default = None
+    else:
+        new.default = default
+
     new.annotation = Optional[field.annotation]
     return (new.annotation, new)
 
@@ -23,7 +28,9 @@ def make_partial_model(model: Type[_BaseModelT], model_name=None) -> Type[_BaseM
         __base__=model,
         __module__=model.__module__,
         **{
-            field_name: _make_field_optional(field_info)
+            field_name: _make_field_optional(
+                field_info, default=field_info.default
+            )
             for field_name, field_info in model.model_fields.items()
         }
     )
